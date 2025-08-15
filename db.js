510806a80884b4ba86407220595fed99c499f826
rachit224agarwal <agarwalrachit224@gmail.com>
2025-08-16 03:00:07 +0530

    add input validations_hashed password

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const ObjectId = mongoose.ObjectId
 
 const User = new Schema({
     name : String,
-    email : String,
+    email : { type : String , unique : true},
     password : String
 })
 
--- index.js
+++ index.js
@@ -1,27 +1,57 @@
+require("dotenv").config();
+const bcrypt = require("bcrypt");
 const express = require("express");
 const { UserModel , TodoModel } = require("./db");
 const app = express();
 const jwt = require("jsonwebtoken");
 const { default: mongoose } = require("mongoose");
-const JWT_SECRET = "ilikeyou";
-mongoose.connect("mongodb+srv://agarwalrachit224:CRISk8Dv9AXRiEL8@cluster0.vyclbuc.mongodb.net/todo-webapp-22")
+const { z } = require("zod");
+
+const jwtSecret = process.env.JWT_SECRET;
+const dbUrl = process.env.DB_URL;
+
+mongoose.connect(dbUrl)
 
 app.use(express.json());
 
 app.post("/signup",async function(req,res){
+    const requireBody = z.object({
+        email : z.string().email(),
+        name : z.string(),
+        password : z.string().min(6)
+    })
+
+    const parseDatawithSuccess = requireBody.safeParse(req.body);
+
+    if(!parseDatawithSuccess.success){
+        res.json({
+            message : "Incorrect Credentials",
+            error : parseDatawithSuccess.error
+        })
+        return
+    }
+
     const name = req.body.name;
     const email = req.body.email;
     const password = req.body.password;
 
+    try{  
+    const hashedPassword = await bcrypt.hash(password , 5);
+
     await UserModel.create({
         name : name,
         email : email,
-        password : password
+        password : hashedPassword
     })
 
     res.json({
         message : "You are signed up"
     })
+    }catch(e){
+        res.json({
+            message : "User already exists"
+        })
+    }
 
 });
 
@@ -30,14 +60,22 @@ app.post("/signin",async function(req,res){
     const password = req.body.password;
 
     const user = await UserModel.findOne({
-        email:email,
-        password:password
+        email:email
     })
+
+    if(!user){
+        res.status(403).json({
+            message : "Wrong Email"
+        })
+        return 
+    }
+
+    const passwordMatch = await bcrypt.compare(password , user.password)
     
-    if(user){
+    if(passwordMatch){
         const token = jwt.sign({
             id : user._id.toString()
-        },JWT_SECRET)
+        },jwtSecret)
         res.json({
             token : token
         })
@@ -54,7 +92,7 @@ function auth(req, res, next) {
         if (!token) {
             return res.status(401).json({ message: "Token missing" });
         }
-        const decodedData = jwt.verify(token, JWT_SECRET);
+        const decodedData = jwt.verify(token, jwtSecret);
         req.userId = decodedData.id;
         next();
     } catch (error) {
@@ -90,4 +128,4 @@ app.get("/todos", auth ,async function(req,res){
     }
 });
 
-app.listen(3000);
+app.listen(process.env.PORT);
